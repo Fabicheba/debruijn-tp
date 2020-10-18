f3ed409b553c4a2b0cad9aa811f9cfe7aaedc2b3
Fabisha <fatoumatabinta.barry@outlook.fr>
2020-10-18 21:38:07 +0200

    fonction get_starting, get_sink, get_contigs, save_contigs, std

--- debruijn.py
+++ debruijn.py
@@ -10,9 +10,13 @@ Année : 2020-2021
 """
 
 # Modules utiles
+
 import argparse
 import networkx as net
 import matplotlib.pyplot as plt
+import os
+from statistics import stdev
+
 # Fonctions
 
 def argument():
@@ -118,8 +122,8 @@ def build_graph(dict_kmer):
     ----------
     dict_kmer: dictionnaire d'occurence des kmers
     -------
-    graphe: dico avec comme clé un tuple (prefixe, suffixe)
-                et comme valeur le poids(occurence du kmer)
+    graphe: graphe avec le module Networkx(prefixe, suffixe)
+                et comme poids l'occurence du kmer
 
     """
     graphe=net.DiGraph()
@@ -129,22 +133,126 @@ def build_graph(dict_kmer):
         poids = dict_kmer[i]
         graphe.add_edge(prefixe, suffixe, weight= poids)
     net.draw(graphe)
-    plt.show()
+    #plt.show()
     return graphe
 
 
 # 2 Parcours du graphe de de Bruijn
 
-"""def get_starting_nodes(graphe):
-    pref_suff=list(graphes.keys())
-    entree=[]
-    for i in len(range(pref_suf)-1):
-        for j in len(range(1,pref_suff)):
-            if pref_suff[i][0] != pref_suff[j][0]"""
+def get_starting_nodes(graphe):
+    """" fonction qui prend en entrée un graphe et 
+        retourne une liste de noeuds d'entrée
+    Parametres
+    ----------
+    graphe: graph de Networkx
+    -------
+    entree: liste des noeuds d'entree
+
+    """
+    noeuds_entree = []
+    for noeud in graphe.nodes:
+        if len(list(graphe.predecessors(noeud))) == 0:
+            noeuds_entree.append(noeud)
+    return noeuds_entree
+
+
+
+def get_sink_nodes(graphe):
+    """" fonction qui prend en entrée un graphe et 
+        retourne une liste de noeuds de sortie
+    Parametres
+    ----------
+    graphe: graph de Networkx
+    -------
+    sortie: liste des noeuds de sortie
+
+    """
+    noeuds_sortie = []
+    for noeud in graphe.nodes:
+        if len(list(graphe.successors(noeud))) == 0:
+            noeuds_sortie.append(noeud)
+    return noeuds_sortie
+
+
+def get_contigs(graphe, noeuds_entree, noeuds_sortie):
+    """" fonction qui prend un graphe, une liste de noeuds
+        d’entrée et une liste de sortie et retourne une 
+        liste de tuple(contig, taille du contig)
+    Parametres
+    ----------
+    graphe: graph de Networkx
+    noeuds_entree: liste des noeuds d'entree
+    noeuds_sortie: liste des noeuds de sortie
+    Return
+    -------
+    contigs: liste de tuple (contig, taille du contig)
+
+    """
+    liste_contigs = []
+    for entree in noeuds_entree:
+        for sortie in noeuds_sortie:
+            for chemin in net.all_simple_paths(graphe, source=entree, target=sortie):
+                contig = chemin[0]
+                for i in range(1, len(chemin)):
+                    contig += chemin[i][-1]
+                liste_contigs.append((contig, len(contig)))
+    return liste_contigs
+        
+
+def fill(text, width=80):
+    """Split text with a line return to respect fasta format"""
+    return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
+
+
+def save_contigs(liste_contigs, contigs_file):
+    with open(contigs_file, "w") as f_contig:
+        num_contig = 0
+        for contig in liste_contigs:
+            num_contig += 1
+            f_contig.write{">contig_{}  len = {}\n".format(num_contig, contig[1])}
+            f_contig.write(fill(contig[0]) + "\n")
+
+
+# 3. Simplification du graphe de de Bruijn
+# a. Résolution des bulles
+
+def std(val_liste):
+    """" fonction qui prend une liste de valeurs et
+         qui retourne l'ecart type
+    Parametres
+    ----------
+    val_list: liste de valeurs
+    Return
+    -------
+    ecart_type: ecart type de la valeur de la liste en entree
+    
+    """
+    return stdev(val_liste)
+
+
+def path_average_weigth(graphe, chemin):
+    """" fonction qui prend un graphe et un chemin et
+        retourne un poids moyen
+    Parametres
+    ----------
+    graphe: Graphe Networkx, issu de la fonction build_graphe
+    chemin: chemin dans le graphe
+    Return
+    -------
+    poids_moyen: poids moyen de tous les chemins
+
+    """
+    poids = 0
+    for i in range(len())
 
 # Programme principal
 if __name__ == "__main__":
     fastq, k, contig = argument()
     kmer = build_kmer_dict(fastq, k)
-    print(kmer)
-    print(build_graph(kmer))
+    #print(kmer)
+    graphe= build_graph(kmer)
+    entree=get_starting_nodes(graphe)
+    sortie= get_sink_nodes(graphe)
+    contig = get_contigs(graphe, entree, sortie)
+    print(entree, sortie)
+    print(contig)
